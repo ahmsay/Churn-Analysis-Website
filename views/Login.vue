d9d4d7df3840d61b3aafd79eba67f12238ffba2a
Ahmet Say <ahmsay@gmail.com>
2019-01-29 12:35:11 +0300

    lorem removed

--- views/Login.vue
+++ views/Login.vue
@@ -59,7 +59,6 @@
         register: false,
         login: false
       },
-      lorem: 'Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.',
       dialog: false,
       unameL: '',
       passwL: '',
